d0c7756d2f42633f1d4440ce0caee7427d7a3aac
sydjefel <sydjefel@ada.local.isima.fr>
2023-10-12 17:28:14 +0200

    enlever les if imbriquées

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -48,14 +48,12 @@ class GildedRose {
     private void updateBackstagePasses(Item item) {
         if (item.quality < 50) {
             item.quality += 1;
-
-            if (item.sellIn < 11 && item.quality < 50) {
-                item.quality += 1;
-            }
-
-            if (item.sellIn < 6 && item.quality < 50) {
-                item.quality += 1;
-            }
+        }
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+        }
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality += 1;
         }
 
         item.sellIn -= 1;
