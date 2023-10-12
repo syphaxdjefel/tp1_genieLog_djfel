957a494d3815fa6ca6706d77c5ccd8f123c020d9
sydjefel <sydjefel@ada.local.isima.fr>
2023-10-12 14:15:04 +0200

    remplacer les if imbriqués avec un et logique et introduire les operateurs += -=

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,90 +1,79 @@
 package com.gildedrose;
+
 class GildedRose {
-  Item[] items;
+    Item[] items;
 
-  public GildedRose(Item[] items) {
-    this.items = items;
-  }
-
-  public void updateQuality() {
-    for (int i = 0; i < items.length; i++) {
-      updateQualityForItem(items[i]);
-    }
-  }
-
-  private void updateQualityForItem(Item item) {
-
-    switch (item.name) {
-    case "Aged Brie":
-      updateAgedBrie(item);
-      break;
-
-    case "Backstage passes to a TAFKAL80ETC concert":
-      updateBackstagePasses(item);
-      break;
-
-    case "Sulfuras, Hand of Ragnaros":
-      updateSulfuras(item);
-      break;
-
-    default:
-      updateDefault(item);
-      break;
-    }
-  }
-
-  private void updateAgedBrie(Item item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
+    public GildedRose(Item[] items) {
+        this.items = items;
     }
 
-    item.sellIn = item.sellIn - 1;
-
-    if (item.sellIn < 0) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
-  }
-
-  private void updateBackstagePasses(Item item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-
-      if (item.sellIn < 11) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            updateQualityForItem(items[i]);
         }
-      }
+    }
 
-      if (item.sellIn < 6) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
+    private void updateQualityForItem(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                updateAgedBrie(item);
+                break;
+
+            case "Backstage passes to a TAFKAL80ETC concert":
+                updateBackstagePasses(item);
+                break;
+
+            case "Sulfuras, Hand of Ragnaros":
+                // Ne rien faire, car Sulfuras ne change jamais
+                break;
+
+            default:
+                updateDefault(item);
+                break;
         }
-      }
     }
 
-    item.sellIn = item.sellIn - 1;
-    if (item.sellIn < 0) {
-      item.quality = 0;
-    }
-  }
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
 
-  private void updateSulfuras(Item item) {
-    // Ne rien faire, car Sulfuras ne change jamais
-  }
+        item.sellIn -= 1;
 
-  private void updateDefault(Item item) {
-    if (item.quality > 0) {
-      item.quality = item.quality - 1;
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality += 1;
+        }
     }
 
-    item.sellIn = item.sellIn - 1;
+    private void updateBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
 
-    if (item.sellIn < 0) {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1;
-      }
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality += 1;
+            }
+
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality += 1;
+            }
+        }
+
+        item.sellIn -= 1;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
     }
-  }
+
+    private void updateDefault(Item item) {
+        if (item.quality > 0) {
+            item.quality -= 1;
+        }
+
+        item.sellIn -= 1;
+
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality -= 1;
+        }
+    }
 }
